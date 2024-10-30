a84f5ccda0f90e02a010888ccf9605c88de00c7d
Diogo Saucedo <Diogoferreiradfs@hotmail.com>
2024-10-30 15:51:00 -0400

    refactor: adiciona polimento ao server

--- server.py
+++ server.py
@@ -1,40 +1,44 @@
 from concurrent import futures
 import grpc
 import time
-
 import proto.chat_pb2 as chat
 import proto.chat_pb2_grpc as rpc
 
 class ChatServer(rpc.ChatServerServicer):
-
     def __init__(self):
-        self.chats = []
+        """Inicializa o servidor de chat com uma lista de mensagens."""
+        self.messages = []  # Armazena mensagens recebidas
 
     def ChatStream(self, request_iterator, context):
-        lastindex = 0
+        """Método para transmitir mensagens de chat para os clientes."""
+        last_index = 0  # Índice para rastrear a última mensagem enviada
         while True:
-            while len(self.chats) > lastindex:
-                n = self.chats[lastindex]
-                lastindex += 1
-                yield n
+            # Envia novas mensagens para os clientes
+            while len(self.messages) > last_index:
+                message = self.messages[last_index]
+                last_index += 1
+                yield message
             time.sleep(0.1)  # Evita busy waiting
 
     def SendNote(self, request: chat.Note, context):
-        print(f"[{request.name}] {request.message}")
-        self.chats.append(request)
-        return chat.Empty()
+        """Recebe uma nova mensagem e a armazena no servidor."""
+        print(f"[{request.name}] {request.message}")  # Exibe a mensagem no console
+        self.messages.append(request)  # Adiciona a mensagem à lista
+        return chat.Empty()  # Retorna uma resposta vazia
 
 if __name__ == '__main__':
     port = 11912
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     rpc.add_ChatServerServicer_to_server(ChatServer(), server)
+    
+    # Inicia o servidor e escuta na porta especificada
     print('Starting server. Listening...')
     server.add_insecure_port(f'[::]:{port}')
     server.start()
 
     try:
         while True:
-            time.sleep(64 * 64 * 100)
+            time.sleep(64 * 64 * 100)  # Mantém o servidor em execução
     except KeyboardInterrupt:
         print("Server stopping...")
-        server.stop(0)
+        server.stop(0)  # Para o servidor de forma limpa
